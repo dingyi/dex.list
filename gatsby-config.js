8370fc422b3dacce4c4859d28e4b054ac42313b1
dingyi <dingyimac@gmail.com>
2019-02-04 02:50:01 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-robots-txt',
+    //'gatsby-plugin-robots-txt',
     'gatsby-transformer-sharp',
     // 'gatsby-plugin-favicon',
     {
