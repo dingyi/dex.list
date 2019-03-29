b3fd8236e6965e9ee1b671a26e13e4ae565bb655
dingyi <dingyimac@gmail.com>
2019-03-29 23:44:31 +0800

    add data

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
         icon: `static/images${config.siteLogo}`
       },
     },
-    'gatsby-plugin-sitemap',
+    `gatsby-plugin-advanced-sitemap`,
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
   ],
