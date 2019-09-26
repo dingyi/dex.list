0ed78d4d8dd4e53e011811a5c47cee944ff8be5d
dingyi <dingyimac@gmail.com>
2019-09-26 19:31:32 +0800

    2.1.1
    add Google AsSense

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,12 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+import React from "react"
+export function onRenderBody(
+  { setHeadComponents }
+) {
+ setHeadComponents([
+     <script
+        data-ad-client="ca-pub-8680661543036255"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />,
+  ]);
+}
